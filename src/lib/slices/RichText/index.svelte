61df7a18d85c10a2c2c9a8feccc5adaba1b81174
Katy Rosli <katyrosli@gmail.com>
2024-08-18 12:38:52 +0200

    set up tailwind

--- src/lib/slices/RichText/index.svelte
+++ src/lib/slices/RichText/index.svelte
@@ -6,7 +6,7 @@
 	export let slice: Content.RichTextSlice;
 </script>
 
-<section class="container">
+<section class="container text-red-500">
 	<PrismicRichText
 		field={slice.primary.content}
 		components={{
