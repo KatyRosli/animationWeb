df33fe21f4a7331d189cf90a2b305d8449201452
Katy Rosli <katyrosli@gmail.com>
2024-08-18 13:15:42 +0200

    set up footer

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,1 +1,13 @@
+import { createClient } from '$lib/prismicio';
+
 export const prerender = 'auto';
+
+export async function load({ fetch, cookies }: { fetch: never; cookies: never }) {
+	const client = createClient({ fetch, cookies });
+
+	const settings = await client.getSingle("settings")
+
+	return {
+		settings: settings,
+	};
+}
